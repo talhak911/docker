06c4f1a95832a4fa9fde6b2a83a9c6a6cf7a7943
talhak911 <talhakhan8300@gmail.com>
2024-05-25 14:19:04 +0500

    kafka consumer

--- kafka-messaging/microservice_01/app/main.py
+++ kafka-messaging/microservice_01/app/main.py
@@ -12,6 +12,26 @@ class Todo1(SQLModel,table=True):
     id:int=Field(primary_key=True)
     contant:str=Field(index=True)
 
+async def consume_messages(topic, bootstrap_servers):
+    # Create a consumer instance.
+    consumer = AIOKafkaConsumer(
+        topic,
+        bootstrap_servers=bootstrap_servers,
+        group_id="my-todos-group",
+        auto_offset_reset='earliest'
+    )
+
+    # Start the consumer.
+    await consumer.start()
+    try:
+        # Continuously listen for messages.
+        async for message in consumer:
+            print(f"Received message: {message.value.decode()} on topic {message.topic}")
+            # Here you can add code to process each message.
+            # Example: parse the message, store it in a database, etc.
+    finally:
+        # Ensure to close the consumer when done.
+        await consumer.stop()
 
     
 
@@ -21,7 +41,9 @@ async def lifespan(app: FastAPI)-> AsyncGenerator[None, None]:
     # loop.run_until_complete(consume_messages('todos', 'broker:19092'))
     # task = asyncio.create_task(consume_messages('todos', 'broker:19092'))
     create_db_and_tables()
+    task = asyncio.create_task(consume_messages('my_todo', 'broker:19092'))
     yield
+
 
 
 app = FastAPI(lifespan=lifespan)
@@ -40,8 +62,10 @@ def home():
 
 @app.post('/create_todo')
 async def send_one(todo:Todo1,session:Annotated[Session,Depends(get_session)],producer:Annotated[AIOKafkaProducer,Depends(get_kafka_prodcer)]):
-
-    return {"message":"success"}
+    todo_json = todo.model_dump_json().encode("utf-8")
+    await producer.send_and_wait("my_todo", todo_json)
+    return {"message": "success"}
+ 
 
 @app.post("/create_todo")
 def create_todo(todo:Todo1,session:Annotated[Session,Depends(get_session)]):
